c301721dc486048a7341235d235792f60411d64d
Allan Aristides <108237959+Aristides05@users.noreply.github.com>
2024-05-22 16:33:49 -0300

    Testando algumas features do selenium

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 const { Builder, Browser, Key, By, Options } = require("selenium-webdriver");
 const chrome = require('selenium-webdriver/chrome');
 
-var options = new chrome.Options();
+const DEFAULT_DIR = "user-data-dir=C:\\Users\\allan\\AppData\\Local\\Google\\Chrome\\User Data";
+PROFILES = ["profile-directory=Profile 3", "profile-directory=Profile 4", "profile-directory=Profile 5", "profile-directory=Profile 7"];            
+                //GOMES = Profile 3     /       MODAS = Profile 4       /       ROCCO = Profile 5       /       STORE = Profile 7
 
-options.addArguments("user-data-dir=C:\\Users\\allan\\AppData\\Local\\Google\\Chrome\\User Data");
-options.addArguments("profile-directory=Profile 5");
 
 
 async function run(){
-    navegador = await new Builder()
-        .forBrowser(Browser.CHROME)
-        .setChromeOptions(options)
-        .build();
+    //for (let i = 0; i < PROFILES.length; i++){
+        //let i = 1; 
+        var options = new chrome.Options();
+        options.addArguments("user-data-dir=C:\\Users\\allan\\AppData\\Local\\Google\\Chrome\\User Data");
+        options.addArguments("profile-directory=Profile 3");
 
-    navegador.get("https://www.youtube.com/"); 
+        let navegador = await new Builder()
+            .forBrowser(Browser.CHROME)
+            //.setChromeOptions(options)
+            .build();
+
+        await navegador.get("https://sellercentral.amazon.com.br/home"); 
+    
+   // }
 }
 
 run();
