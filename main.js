0941e099e2aef261bf5344fb7e9508ce3d65d823
Allan Aristides <108237959+Aristides05@users.noreply.github.com>
2024-05-24 23:35:32 -0300

    Finalização da automação em um perfil

--- main.js
+++ main.js
@@ -4,7 +4,27 @@ const firefox = require('selenium-webdriver/firefox');
 const DEFAULT_DIR = "C:\\Users\\allan\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles";
 const PROFILE = "wmtulw9d.STORE";
 
-async function run() {
+const CHECK_SPREADSHEET = ["#includeZeroImpressionEntities", 
+                           "#includeSponsoredBrandsData",
+                           "#includeSponsoredBrandsV4Data",
+                           "#includeSponsoredDisplayData", 
+                           "#includeSbSearchTermReportData"];
+
+const SELECTORS_1 = ["span.sc-storm-ui-30041982__sc-may1nv-1:nth-child(4) > button:nth-child(1)",
+                   ".sc-storm-ui-30041982__sc-amnlzo-2", 
+                   "button.sc-storm-ui-30041982__sc-1a042f9-1:nth-child(1)", 
+                   ".sc-pcLhl"];
+
+const SELECTORS_2 = ["button.sc-storm-ui-20042007__sc-d3w8xm-0:nth-child(2)",
+                     "button.sc-storm-ui-20042007__sc-1rm1drt-0:nth-child(5)", 
+                     "button.dQwwDk:nth-child(2)", 
+                     ".dQwwDk"];
+
+async function main(){
+     runAuto();
+}
+
+async function runAuto(){
     var options = new firefox.Options();
     options.setProfile(`${DEFAULT_DIR}\\${PROFILE}`);
 
@@ -13,32 +33,29 @@ async function run() {
         .setFirefoxOptions(options)
         .build();
 
-        await navegador.manage().setTimeouts({ implicit: 2000 });
-        
-    await navegador.get("https://sellercentral.amazon.com.br/home");
-    const actions = navegador.actions({async: true});
-    clickComponent("nav-button-icon sc-nav-kat-icon sc-nav-kat-icon-hamburger sc-nav-kat-icon-small"); //menu
-    let anuncios = navegador.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div[3]/div[3]/div/div[1]/div[2]/div/div[4]"));
-    await actions.move({origin: anuncios}).perform();
-    let campaing = navegador.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div[3]/div[3]/div/div[1]/div[2]/div/div[4]/div[2]/div[2]"))
-    await actions.move({origin: campaing}).click().perform();
-    test("/html/body/div[1]/div[4]/div/div/div[1]/div[3]/div[1]/span/button"); //icon
-    let buk_operations = navegador.findElement(By.xpath("/html/body/div[1]/div[4]/div[1]/div/div[1]/div[3]/div[2]/div[2]/div[2]"));
-    await buk_operations.click();
-    let language = navegador.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/header/div[3]/span[4]/button"));
-    await language.click();
-    let select = navegador.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[2]/div/button"));
-    await select.click();
+    await navegador.get("https://advertising.amazon.com.br/bulksheet/HomePage");
+    await pathAuto(SELECTORS_1);
+    await checksSpreadsheet(CHECK_SPREADSHEET); 
+    await pathAuto(SELECTORS_2);
     //await navegador.quit();
 
-    function clickComponent(className){
-        let component = navegador.findElement(By.className(className));
-        component.click();
+    async function checksSpreadsheet(selectors){
+        for(let i = 0; i < selectors.length; i++){
+            await clickComponent(selectors[i]);
+        }
     }
 
-    function moveMouse(){
-        
+    async function pathAuto(selectors){
+        for (let i = 0; i < selectors.length; i++){
+            await clickComponent(selectors[i]);
+        }
     }
+
+    async function clickComponent(className){
+        let component = navegador.findElement(By.css(className));
+        await component.click();
+    } 
 }
 
-run();
+
+main();
